1857f02cb157830a64104166ba0b509bdd0cb362
Michael <kasmickleva@gmail.com>
2018-11-25 14:37:04 +0100

    feat: fetch geographical information of current location

--- template/js/geoNotification.js
+++ template/js/geoNotification.js
@@ -1,27 +1,66 @@
-
-// simulate an event
-const customEvent = (event) => {
-  event.preventDefault();
-  
-  if (!window.Notification) alert('Sorry Notifications are not supported on this browser');
-  Notification.requestPermission();
-
-  let notify;
-  if (Notification.permission === 'denied') alert('You have to allow notification to receive notifications');
-  notify = new Notification('New message from Alex', {
-    body: 'How are you today, it realy is a lovely day',
-    icon: '../images/246x0w.jpg',
-    tag: '1234'
-  });
-
-  notify.onclick = function(e){
-    window.location= `?messages=${this.tag}`;
+function initMap() {
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
   }
-} 
-
-function notification() {
-  const bell = document.querySelector('.fas.fa-bell');
-  bell.addEventListener('click', customEvent)
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      const config = { zoom: 17, center: pos }
+      const infoWindow = new google.maps.InfoWindow;
+      const map = new google.maps.Map(document.querySelector('#map'), config);
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+  
+      const checkAddress = document.querySelector('.address')
+        .addEventListener('click', getLocation);
+  
+      async function getLocation(event) {
+        event.preventDefault();
+        const address = document.querySelector('#address').value;
+        if (address === '') {
+          console.log('Address field is empty');
+          return;
+        }
+  
+        try {
+          const data = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+              address,
+              key: 'AIzaSyB0rESKxxyOCdD5o7BEiUKwHW-hErMDMPI'
+            }
+          });
+          const { location } = data.data.results[0].geometry;
+          let newPos = location ? { lat: location.lat, lng: location.lng } : undefined;
+          if(newPos) {
+            infoWindow.setPosition(newPos);
+            map.setCenter(newPos);
+          } else {
+            infoWindow.setContent('Error: The Geolocation service failed.')
+          }
+          infoWindow.open(map);
+  
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    }, options)
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter())
+  }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
 }
-
-notification();
