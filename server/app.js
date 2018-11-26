18a2c48f933a52341de56f2abd62220d22a8db1d
Michael <kasmickleva@gmail.com>
2018-11-26 14:33:56 +0100

    refactor: change api port number

--- server/app.js
+++ server/app.js
@@ -5,21 +5,24 @@ import path from 'path';
 import jsend from 'jsend';
 import cors from 'cors';
 import helmet from 'helmet';
+import routers from './routes/index';
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/template')));
+app.use(helmet());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '..', '/template')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(jsend.middleware);
 app.use(morgan('combined'));
-app.use(cors());
-app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Ireporter Api');
 });
 
-const port = process.env.PORT || 8080;
+routers(app);
+
+const port = process.env.PORT || 2080;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
