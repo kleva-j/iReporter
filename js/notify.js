fa34b7e71713630f70330c0a41d0403fc6a190e2
Michael <kasmickleva@gmail.com>
2018-12-06 02:52:02 +0100

    fix(UI): Implement feedback on UI - customize buttons to update redflag status - add appropriate navigation links to all ui components - increase font size of small illegible text [finishes]

--- js/notify.js
+++ js/notify.js
@@ -2,7 +2,7 @@
 // simulate an event
 const customEvent = (event) => {
   event.preventDefault();
-  
+
   if (!window.Notification) alert('Sorry Notifications are not supported on this browser');
   Notification.requestPermission();
 
@@ -11,17 +11,17 @@ const customEvent = (event) => {
   notify = new Notification('New message from Alex', {
     body: 'How are you today, it realy is a lovely day',
     icon: '../images/246x0w.jpg',
-    tag: '1234'
+    tag: '1234',
   });
 
-  notify.onclick = function(e){
-    window.location= `?messages=${this.tag}`;
-  }
-} 
+  notify.onclick = function (e) {
+    window.location = `?messages=${this.tag}`;
+  };
+};
 
 function notification() {
   const bell = document.querySelector('.fas.fa-bell');
-  bell.addEventListener('click', customEvent)
+  bell.addEventListener('click', customEvent);
 }
 
 notification();
