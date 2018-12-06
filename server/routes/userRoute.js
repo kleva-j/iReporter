0684a109e68a59473a87743a5e97073c028c15e7
Michael <kasmickleva@gmail.com>
2018-12-06 03:06:06 +0100

    chore(server): write test for api endpoints - write test for the signup and signin endpoints - provide route to update a red-flag status [finishes]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const userController = require('../controllers/userController');
 const userValidator = require('../utils/userValidator');
 
@@ -16,14 +15,9 @@ const {
 const userRouter = express.Router();
 
 userRouter.route('/login')
-  .get((req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '..', '..', 'template', 'html', 'login.html'));
-  })
   .post(validateLogin, LoginUser);
 
 userRouter.route('/signup')
-  .get((req, res) => res.status(200).sendFile(path.join(__dirname, '..', '..', 'template', 'html', 'signup.html')))
   .post(validateSignup, RegisterUser);
-
 
 module.exports = userRouter;
