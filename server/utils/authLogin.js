071576924743181c5555ea7cdfd6901598eab90c
Michael <kasmickleva@gmail.com>
2018-12-13 11:16:36 +0100

    feat(server): create functions to authenticate user credentials

--- server/utils/authLogin.js
+++ server/utils/authLogin.js
@@ -3,14 +3,26 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-export default (req, res, next) => {
+const authToken = (req, res, next) => {
   const token = req.headers['x-access-token'] || req.body.token || req.query.token;
-  if (!token) res.status(401).jsend.fail({ messsage: 'You are required to login to access this' });
-  else {
+  if (!token) {
+    res.status(401).json({
+      status: 401,
+      messsage: 'You are required to login to access this endpoint',
+    });
+  } else {
     jwt.verify(token, process.env.SECRET, (err, response) => {
-      if (err) res.status(401).jsend.fail({ message: 'failed to authenticate' });
+      if (err) {
+        res.status(401).json({
+          status: 401,
+          message: 'failed to authenticate',
+        });
+      }
+
       req.auth = response;
       next();
     });
   }
 };
+
+export default authToken;
